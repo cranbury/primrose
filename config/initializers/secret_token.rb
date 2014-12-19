182a840045a34bf176ef4e89014f775a7506d87b
Granger <GrangerAbuhoff@gmail.com>
2014-12-18 22:45:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Primrose::Application.config.secret_key_base = 'df161a911ebe1c89cf310e8f51132b2cb940bc9eb0f225d91092fc084e85a3c0d9c7e15737ec1d44787ede8279bd88862885b598e17262e684c3e57be2be3321'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Primrose::Application.config.session_store :cookie_store, key: '_primrose_session'
